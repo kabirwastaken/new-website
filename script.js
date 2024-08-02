615b1232495dba1c6be7908946d3494c3b58ffc1
kabirwastaken <168340028+kabirwastaken@users.noreply.github.com>
2024-08-03 03:45:27 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x')
+    navbar.classList.toggle('active');
+}
+
+
+
+var content = document.getElementsByTagName('body')[0];
+        var darkMode = document.getElementById('dark-change');
+        darkMode.addEventListener('click', function(){
+            darkMode.classList.toggle('active');
+            content.classList.toggle('night');
+        })
