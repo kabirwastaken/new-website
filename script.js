63bfc2c7fc0b7f4a2fcc131c720f4bdcf69b40b9
Faiaz-Hossain <148471163+Faiaz-Hossain@users.noreply.github.com>
2024-09-02 02:02:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let menuIcon=document.querySelector('#menu-icon');
-let navbar=document.querySelector('.navbar');
+let navbar=document.querySelector('.nav-items');
 
 menuIcon.onclick=()=>{
     menuIcon.classList.toggle('bx-x')
